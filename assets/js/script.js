56b457ef331e8d8eece8a75f0247d478642e6dbc
balgxmr <jose@pixelos.net>
2024-09-13 20:16:07 -0500

    Javascript/Card-Total: Calcular el total de la card.
    * Utilizando querySelector(), el evento onClick() y lógica de programación.

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Inicialización de variables
+const precio = 400000; // En este caso el precio será fijo, pero podría ser let o var en caso de que nuestro sitio lo requiera
+const precioSpan = document.querySelector(".precio-inicial");
+const cantidadSpan = document.querySelector(".cantidad");
+const precioTotalSpan = document.querySelector(".valor-total");
+const btnAumentar = document.querySelector("#btnAumento");
+const btnDecremento = document.querySelector("#btnDecremento");
+
+let cantidad = 0;
+
+// Muestra el precio del producto
+precioSpan.innerHTML = precio;
+
+// Funcion para actualizar el valor total mostrado
+function actualizarTotal() {
+  precioTotalSpan.innerHTML = cantidad * precio;
+}
+
+// Aumentar cantidad en evento click al botón y llamar a actualizarTotal()
+btnAumentar.addEventListener("click", () => {
+  cantidad += 1;
+  cantidadSpan.innerHTML = cantidad;
+  actualizarTotal();
+});
+
+// Decrementar cantidad en evento click al botón y llamar a actualizarTotal()
+btnDecremento.addEventListener("click", () => {
+  cantidad -= 1;
+  cantidadSpan.innerHTML = cantidad;
+  actualizarTotal();
+});
